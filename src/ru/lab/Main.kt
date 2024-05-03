d9d0408418582885e816b8ada75459c40cd5628c
meinsteren <meinsteren@gmail.com>
2024-05-03 17:42:00 +0300

    first commit

--- src/ru/lab/Main.kt
+++ src/ru/lab/Main.kt
@@ -1,5 +1,46 @@
 package ru.lab
 
-fun main() {
-    println("Hello World!")
+interface FuelEfficient {
+    fun fuelEfficiency()
 }
+
+open class Vehicle(val speed: Int, val color: String) {
+    open fun displayInfo() {
+        println("Транспортное средство - Скорость: $speed, Цвет: $color")
+    }
+}
+
+open class Car(speed: Int, color: String, val numOfDoors: Int) : Vehicle(speed, color) {
+    override fun displayInfo() {
+        println("Машина - Скорость: $speed, Цвет: $color, Количество дверей: $numOfDoors")
+    }
+}
+
+class Bike(speed: Int, color: String, val countOfWheels: Int) : Vehicle(speed, color) {
+    override fun displayInfo() {
+        println("Велосипед - Скорость: $speed, Цвет: $color, Количество колес: $countOfWheels")
+    }
+}
+
+class ElectricCar(speed: Int, color: String, numOfDoors: Int, val batteryCapacity: Int) : Car(speed, color, numOfDoors), FuelEfficient {
+    override fun displayInfo() {
+        super.displayInfo()
+        println("Электромобиль - Емкость батареи: $batteryCapacity kWh")
+    }
+
+    override fun fuelEfficiency() {
+        println("Этот электромобиль энергоэфективен.")
+    }
+}
+
+fun main() {
+    val car = Car(200, "Черный", 4)
+    val bike = Bike(20, "Красный", 2)
+    val electricCar = ElectricCar(120, "Белый", 4, 75)
+
+    car.displayInfo()
+    bike.displayInfo()
+    electricCar.displayInfo()
+    electricCar.fuelEfficiency()
+}
+
